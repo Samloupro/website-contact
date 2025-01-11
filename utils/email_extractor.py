5a006c5360590dba1d034697b71b51c7b0690792
Samloupro <54007076+Samloupro@users.noreply.github.com>
2025-01-11 21:01:33 +0100

    Update email_extractor.py
    remove emails with subdomains

--- utils/email_extractor.py
+++ utils/email_extractor.py
@@ -2,7 +2,8 @@ import re
 import json
 
 def extract_emails_html(text):
-    return re.findall(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+', text)
+    # Updated regex pattern to exclude emails with subdomains
+    return re.findall(r'[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z]{2,}(?!\.\w+)', text)
 
 def extract_emails_jsonld(soup):
     emails = []
@@ -23,6 +24,6 @@ def extract_valid_emails(text, soup):
     jsonld_emails = extract_emails_jsonld(soup)
     # Combine both lists
     all_emails = html_emails + jsonld_emails
-    # Validate emails using regex
-    valid_emails = [email for email in all_emails if re.match(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email)]
+    # Validate emails using regex and exclude specific patterns
+    valid_emails = [email for email in all_emails if re.match(r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email) and not re.match(r'^[a-f0-9]{32}@sentry\.zipify\.com$', email)]
     return valid_emails
